7bfe1af86631594c606d7a3e2f4a3962dcd63d82
rrishavj <rishavj@lambdatest.com>
2025-06-06 16:12:18 +0530

    Clean Documentation Repo

--- ios/example/AppDelegate.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-#import <RCTAppDelegate.h>
-
-@interface AppDelegate : RCTAppDelegate
- 
-@end 
--- metro.config.js
+++
@@ -1,6 +0,0 @@
-const {getDefaultConfig} = require('@react-native/metro-config');
- 
-module.exports = (async () => {
-  const config = await getDefaultConfig(__dirname);
-  return config;
-})(); 
